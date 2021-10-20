a8bfbb88278b03cd0e7ef6db724b4b74c17de5fb
TotpalIstvan <totpal.istvan@petrik.hu>
2021-10-20 11:19:09 +0200

    edit php fajlhoz change

--- editBlogPost.php
+++ editBlogPost.php
@@ -3,6 +3,13 @@
 require_once 'db.php';
 require_once 'Bejegyzes.php';
 
+$bejegyzesId = $_GET['id'] ?? null;
+
+if ($bejegyzesId === null) {
+    header('Location: index.php');
+    exit();
+}
+
 ?><!DOCTYPE html>
 <html lang="hu">
 <head>
