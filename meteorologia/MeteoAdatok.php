02b0bc4c2d03702016addde94afcf5a55d129fbb
Gergely Pivarcsi <gergely.pivarcsi@gmail.com>
2021-10-20 11:47:17 +0200

    initial commit

---
+++ meteorologia/MeteoAdatok.php
@@ -0,0 +1,66 @@
+<?php
+
+class MeteoAdatok {
+    private $id;
+    private $tartalom;
+    private $datum;
+
+    public function __construct(string $tartalom, DateTime $datum) {
+        $this->tartalom = $tartalom;
+        $this->datum = $datum;
+    }
+
+    // A létrehozott blog bejegyzést menti az adatbazisba.
+    // Hiányosság: az ID-t nem állítja be!
+    public function uj() {
+        global $db;
+
+        // Paraméteres lekérdezéskor a ->prepare fv-t kell használni!
+        $db->prepare('INSERT INTO bejegyzesek (tartalom, datum)
+                    VALUES (:tartalom, :datum)')
+            ->execute([
+                ':tartalom' => $this->tartalom,
+                ':datum' => $this->datum->format('Y-m-d H:i:s'),
+            ]);
+    }
+
+    public function getId() : ?int { // ? -> lehet null is az értéke
+        return $this->id;
+    }
+
+    public function getTartalom() : string {
+        return $this->tartalom;
+    }
+
+    public function getDatum() : DateTime {
+        return $this->datum;
+    }
+
+    // Az adott ID-jű bejegyzést törli. Statikus!
+    public static function torol(int $id) {
+        global $db;
+
+        // Paraméteres
+        $db->prepare('DELETE FROM bejegyzesek WHERE id = :id')
+            ->execute([':id' => $id]);
+    }
+
+    // Visszaadja az összes bejegyzést. Statikus!
+    public static function osszes() : array {
+        // Rossz szokás, a későbbiekben tanulunk rá jobbat
+        global $db;
+
+        $t = $db->query("SELECT * FROM bejegyzesek ORDER BY datum DESC")
+            ->fetchAll();
+        $eredmeny = [];
+
+        foreach ($t as $elem) {
+            $bejegyzes = new Bejegyzes($elem['tartalom'],
+                                       new DateTime($elem['datum']));
+            $bejegyzes->id = $elem['id'];
+            $eredmeny[] = $bejegyzes;
+        }
+
+        return $eredmeny;
+    }
+}
---
+++ meteorologia/index.php
@@ -0,0 +1,22 @@
+<?php 
+
+
+
+
+?><!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post"></form>
+
+    <?php 
+    
+    foreach ($variable as $key) {
+        
+    }
+    ?>
+</body>
+</html>
